a1e3012ecc1941528156d09d22d07afc9d5edc77
kota-batu <paradiseuniversetour@gmail.com>
2026-08-23 21:11:03 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -71,7 +71,7 @@ const POST_CONTENT_TYPE = 'text/plain;charset=utf-8'; // hindari CORS preflight 
  * URL Web App hasil Deploy Apps Script.
  ******************************************************************/
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyB97R-8HJicXK7sBqxLDEBNMoYBLmqYI43XSxuiKu495pnCoUeQQan2JgmJ6Y5rYEyXA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwUF5VFRo2t30Jd4l_Y_sClvHoZUEi0fhWL04wtHtLssLYJteQyUd4XkKejskFJleZVJg/exec';
 
 /******************************************************************
  * HTTP HELPERS
